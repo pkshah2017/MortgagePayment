5e5515761ab31e9ab5868b2dd518c3f018247bf7
Parshva Shah <pkshah2017@gmail.com>
2023-08-21 20:32:27 -0400

    Add MUI Table to render payment plan

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import LoadDetailStoreProvider from "@components/LoadDetailStoreProvider";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 export default function MyApp({ Component, pageProps }) {
     return (
         <LoadDetailStoreProvider>
